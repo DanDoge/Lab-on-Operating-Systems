f47fb9a01c3747a98fbbf1717f66dbc2ef4b10f5
DanDoge <30900990+DanDoge@users.noreply.github.com>
2018-04-04 17:06:28 +0800

    update: 04-04
    404 day

--- 第3次作业/src/script.sh
+++ 第3次作业/src/script.sh
@@ -1,6 +1,6 @@
 # 主机内
 
-# pid为容器的pid
+# pid为容器的pid, main里面可以获取
 ln -s /proc/$pid/ns/net /var/run/netns/$pid
 
 brtcl addbr bridge_0
@@ -15,19 +15,28 @@ ifconfig vethB up
 
 ip link set vethC netns $pid_t
 
-ip netns $pid exec ifconfig vethC $container netmask 255.255.255.0
+ip netns exec $pid ip link set dev vethC name eth0
+
+ip netns $pid exec ifconfig vethC $container netmask 255.255.255.0 up
 
 # 至此, 网络结构应该是建立完毕了
+# 主机在这里能ping通容器
 
 # 访问网络
+
+# 这一句可能是没有用的, 但是至少能ping通主机, 但是不能上外网
+# 似乎这句只让所有包从eth0发出去, 网桥自然可以看到, 主机也能看到
 
 ip netns $pid exec ip route add default dev eth0
 
 iptables -t nat -A POSTROUTING -s $container -j SNAT --to $host
 
-ip netns $pid exec route add default gw $host dev vethC
+# 可以上网了, 所有包的下一跳是主机(连接了外网)
+# (host是内网地址还是公网地址来着...好像用内网地址也行...吗?)
 
-# 端口映射
+ip netns $pid exec route add default gw $host dev eth0
+
+# 端口映射, 假设这里宿主机打开了转发
 
 iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination $container:80
 
