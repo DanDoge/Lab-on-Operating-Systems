c34f1f6afdff4495c387261e7cfc5960dc127eb7
DanDoge <30900990+DanDoge@users.noreply.github.com>
2018-05-25 21:44:45 +0800

    update: 05-25
    aufs挂了...开容器慢到要死...

---
+++ 第6次作业/src/config_ip.sh
@@ -0,0 +1,30 @@
+rm /var/lib/lxc/${1}/config
+cat << EOF > /var/lib/lxc/${1}/config
+# Template used to create this container: /usr/share/lxc/templates/lxc-ubuntu
+# Parameters passed to the template:
+# Template script checksum (SHA-1): 4d7c613c3c0a0efef4b23917f44888df507e662b
+# For additional config options, please look at lxc.container.conf(5)
+# Uncomment the following line to support nesting containers:
+#lxc.include = /usr/share/lxc/config/nesting.conf
+# (Be aware this has security implications)
+# Common configuration
+lxc.include = /usr/share/lxc/config/ubuntu.common.conf
+# Container specific configuration
+lxc.rootfs = /var/lib/lxc/${1}/rootfs
+lxc.rootfs.backend = dir
+lxc.utsname = ${1}
+lxc.arch = amd64
+# Network configuration
+lxc.network.type = veth
+lxc.network.name = eth0
+lxc.network.link = lab6br0
+lxc.network.ipv4 = 172.16.100.${2}/24
+lxc.network.ipv4.gateway = 172.16.100.1
+lxc.network.veth.pair = ${1}
+lxc.network.flags = up
+EOF
+rm /var/lib/lxc/${1}/rootfs/etc/hostname
+cat << EOF > /var/lib/lxc/${1}/rootfs/etc/hostname
+${1}
+EOF
+sudo lxc-start -n ${1}
--- 第6次作业/src/parse_input.cpp
+++ 第6次作业/src/parse_input.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <oftream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
   ofstream out("out.txt");
   if(argc < 6){
     cerr << "lose some parameters? in parse_input.cpp" << endl;
+    out << "lose some parameters? in parse_input.cpp" << endl;
   }else{
     char* jobname = argv[1];
     char* cmd_line = argv[2];
@@ -18,6 +21,22 @@ int main(int argc, char const *argv[]) {
     out << "max_retry_time is " << max_retry_time << endl;
     out << "time_out is " << time_out << endl;
     out << "image_id is " << image_id << endl;
+
+    int rand_ip = max(rand() % 256, 2);
+/*
+    char cmd[1024];
+    sprintf(cmd, "%s %s %d", "./aufs_shell.sh", image_id, rand_ip);
+    printf("command line is: %s", cmd);
+    system(cmd);
+*/
+    char cmd[1024] = {};
+    sprintf(cmd, "sudo lxc-create -t ubuntu -n %s", image_id);
+    //system(cmd);
+    sprintf(cmd, "sudo ./config_ip.sh %s %d", image_id, rand_ip);
+    //system(cmd);
+    sprintf(cmd, "sudo lxc-attach -n %s -- %s", image_id, cmd_line);
+    cout << cmd << endl;
+    system(cmd);
   }
   out.close();
   return 0;
